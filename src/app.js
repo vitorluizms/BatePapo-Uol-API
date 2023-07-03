4d7f07fde08bee5ade825a180eab54a7275f6455
vitorluizms <vitor.luiz.eer@gmail.com>
2023-07-03 11:50:17 -0300

    Adição de comentários e organizando o código agrupando requisições

--- src/app.js
+++ src/app.js
@@ -27,8 +27,9 @@ const db = mongoClient.db();
 let participants = [];
 let messages = [];
 
-//Funções (endpoints)
 
+//Funções (endpoints)
+//Requisições POST
 app.post("/participants", async (req, res) => {
   let { name } = req.body;
   const schemaParticipant = Joi.object({
@@ -68,14 +69,6 @@ app.post("/participants", async (req, res) => {
   }
 });
 
-app.get("/participants", async (req, res) => {
-  try {
-    const usersList = await db.collection("participants").find().toArray();
-    res.status(200).send(usersList);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
 
 app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
@@ -123,6 +116,46 @@ app.post("/messages", async (req, res) => {
   }
 });
 
+
+app.post("/status", async (req, res) => {
+  const { user } = req.headers;
+  const schemaStatus = Joi.object({
+    user: Joi.string().required(),
+  });
+  const object = { user };
+
+  const validate = schemaStatus.validate(object, { abortEarly: false });
+  if (validate.error) {
+    const errors = validate.error.details.map((detail) => detail.message);
+    return res.status(404).send(errors);
+  }
+  try {
+    const userValid = await db
+      .collection("participants")
+      .findOne({ name: user });
+
+    if (!userValid) return res.status(404).send("Usuário não encontrado!");
+
+    await db
+      .collection("participants")
+      .updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+//Requisições GET
+app.get("/participants", async (req, res) => {
+  try {
+    const usersList = await db.collection("participants").find().toArray();
+    res.status(200).send(usersList);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 app.get("/messages", async (req, res) => {
   const { user } = req.headers;
   const { limit } = req.query;
@@ -160,34 +193,8 @@ app.get("/messages", async (req, res) => {
   }
 });
 
-app.post("/status", async (req, res) => {
-  const { user } = req.headers;
-  const schemaStatus = Joi.object({
-    user: Joi.string().required(),
-  });
-  const object = { user };
 
-  const validate = schemaStatus.validate(object, { abortEarly: false });
-  if (validate.error) {
-    const errors = validate.error.details.map((detail) => detail.message);
-    return res.status(404).send(errors);
-  }
-  try {
-    const userValid = await db
-      .collection("participants")
-      .findOne({ name: user });
-
-    if (!userValid) return res.status(404).send("Usuário não encontrado!");
-
-    await db
-      .collection("participants")
-      .updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
-    res.sendStatus(200);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
-
+//Requisição DELETE
 app.delete("/messages/:id", async (req, res) => {
   const { user } = req.headers;
   const { id } = req.params;
@@ -206,6 +213,8 @@ app.delete("/messages/:id", async (req, res) => {
   }
 });
 
+
+//Requisição PUT
 app.put("/messages/:id", async (req, res) => {
   const { to, type, text } = req.body;
   const { user } = req.headers;
@@ -235,7 +244,7 @@ app.put("/messages/:id", async (req, res) => {
     const message = await db
       .collection("messages")
       .findOne({ _id: new ObjectId(id) });
-      
+
     if (!message) return res.sendStatus(404);
     if (message.from !== user)
       return res.status(401).send("Você não é o dono da mensagem");
@@ -249,28 +258,30 @@ app.put("/messages/:id", async (req, res) => {
   }
 });
 
-// setInterval(async () => {
-//   try {
-//     const participants = await db
-//       .collection("participants")
-//       .find({ lastStatus: { $lt: Date.now() - 10000 } })
-//       .toArray();
 
-//     participants.forEach(async (user) => {
-//       await db.collection("participants").deleteOne({ name: user.name });
+//Função de remoção automática de usuário a cada 15 segundos
+setInterval(async () => {
+  try {
+    const participants = await db
+      .collection("participants")
+      .find({ lastStatus: { $lt: Date.now() - 10000 } })
+      .toArray();
 
-//       await db.collection("messages").insertOne({
-//         from: user.name,
-//         to: "Todos",
-//         text: "sai da sala...",
-//         type: "status",
-//         time: dayjs().locale("pt-br").format("HH:mm:ss"),
-//       });
-//     });
-//   } catch (err) {
-//     res.sendStatus(500).send(err.message);
-//   }
-// }, 15000);
+    participants.forEach(async (user) => {
+      await db.collection("participants").deleteOne({ name: user.name });
+
+      await db.collection("messages").insertOne({
+        from: user.name,
+        to: "Todos",
+        text: "sai da sala...",
+        type: "status",
+        time: dayjs().locale("pt-br").format("HH:mm:ss"),
+      });
+    });
+  } catch (err) {
+    res.sendStatus(500).send(err.message);
+  }
+}, 15000);
 
 const PORT = 5000;
 app.listen(PORT, () => {
